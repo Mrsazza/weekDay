57812d6297b734907d4e0713e9d2b2aeda796006
Mrsazza <shahriarhossain147@gmail.com>
2021-12-19 03:29:43 +0600

    Initial Commit

---
+++ weekDay/weekDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  weekDayApp.swift
+//  weekDay
+//
+//  Created by Sazza on 19/12/21.
+//
+
+import SwiftUI
+
+@main
+struct weekDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
